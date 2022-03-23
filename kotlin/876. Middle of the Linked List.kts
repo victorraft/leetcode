991f0fee62103501bf5b4b14d2235bb9d21c6d4f
Victor Raft <victoraft@gmail.com>
2022-03-23 11:49:39 -0300

    Update 876. Middle of the Linked List.kts

--- kotlin/876. Middle of the Linked List.kts
+++ kotlin/876. Middle of the Linked List.kts
@@ -12,30 +12,16 @@ class ListNode(var `val`: Int) {
     var next: ListNode? = null
 }
 
-// TODO can it be done better?
 class Solution {
     fun middleNode(head: ListNode?): ListNode? {
-        if (head?.next == null) {
-            return head
-        }
-
-        var count = 0
         var lastNode = head
         var middleNode = head
 
-        while (lastNode != null) {
-            count++
-            lastNode = lastNode.next
-            if (count % 2 == 0) {
-                middleNode = middleNode?.next
-            }
+        while (lastNode?.next != null) {
+            middleNode = middleNode?.next
+            lastNode = lastNode?.next?.next
         }
 
         return middleNode
     }
-}
-
-fun test(head: ListNode?) {
-    val middleNode = Solution().middleNode(head)
-    println(middleNode)
 }
