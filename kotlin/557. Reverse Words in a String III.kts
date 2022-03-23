d0d5f435333bf6ecc5e9e93d61485145425af0e1
Victor Raft <victoraft@gmail.com>
2022-03-23 11:24:51 -0300

    Update 557. Reverse Words in a String III.kts

--- kotlin/557. Reverse Words in a String III.kts
+++ kotlin/557. Reverse Words in a String III.kts
@@ -1,7 +1,79 @@
 class Solution {
-    fun reverseWords(s: String): String {
+    fun reverseWordsWithKotlinFunctions(s: String): String {
         return s.split(" ").joinToString(" ") { word -> word.reversed() }
     }
+
+    fun reverseWordsWithCustomFunctions(s: String): String {
+        val words = split(s, ' ')
+
+        return buildString {
+            words.forEachIndexed { index, word ->
+                append(reverseString(word))
+                if (index != words.lastIndex) {
+                    append(' ')
+                }
+            }
+        }
+    }
+
+    fun reverseWords(s: String): String {
+        if (s.isEmpty()) {
+            return s
+        }
+
+        val reversedWords = mutableListOf<String>()
+        val wordBuilder = StringBuilder()
+
+        for (index in s.lastIndex downTo 0) {
+            val char = s[index]
+            if (char == ' ') {
+                reversedWords.add(wordBuilder.toString())
+                wordBuilder.clear()
+            } else {
+                wordBuilder.append(char)
+            }
+        }
+
+        reversedWords.add(wordBuilder.toString())
+
+        return buildString {
+            for (index in reversedWords.lastIndex downTo 0) {
+                append(reversedWords[index])
+                if (index != 0) {
+                    append(' ')
+                }
+            }
+        }
+    }
+
+    private fun split(s: String, delimiter: Char): List<String> {
+        if (s.isEmpty()) {
+            return emptyList()
+        }
+
+        val list = mutableListOf<String>()
+        val wordBuilder = StringBuilder()
+
+        s.forEachIndexed { index, char ->
+            if (char == delimiter) {
+                list.add(wordBuilder.toString())
+                wordBuilder.clear()
+            } else {
+                wordBuilder.append(char)
+            }
+        }
+
+        list.add(wordBuilder.toString())
+
+        return list
+    }
+
+    private fun reverseString(s: String): String =
+        buildString {
+            for (i in s.lastIndex downTo 0) {
+                append(s[i])
+            }
+        }
 }
 
 fun test(s: String, expectedString: String) {
